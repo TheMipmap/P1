295013868bb3415e85dd075d124b4c6dea3254a7
TheMipmap <56264389+TheMipmap@users.noreply.github.com>
2021-11-30 15:12:53 +0100

    Creating the file for our final P1 project
    The code for our P1 project.

---
+++ P1.ino
@@ -0,0 +1,33 @@
+#include <Wire.h>
+#include <Zumo32U4.h>
+
+
+Zumo32U4Motors motors;
+Zumo32U4Encoders encoders;
+Zumo32U4LCD lcd;
+Zumo32U4ButtonA buttonA;
+Zumo32U4LineSensors lineSensors;
+Zumo32U4IMU imu;
+
+//
+//
+//Global variables should be written below this comment
+//
+//
+
+
+void setup() {
+  // Code that runs once before the loop() function
+  Serial.begin(9600);
+}
+
+void loop() {
+  //Code that loops over and over again until the robot stops.
+  
+}
+
+//
+//
+//Functions should be declared below this comment
+//
+//
